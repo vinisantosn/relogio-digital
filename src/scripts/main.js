0cba7c4c6271bd90a8de774223660c8a5707dfde
Vinicius Santos <vinisantoosk@gmail.com>
2023-08-08 23:43:58 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+const miliSegundos = document.getElementById('mili-segundos');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+    let milsec = dateToday.getMilliseconds();
+
+    if(hr<10) hr = '0' + hr;
+    if(min<10) min = '0' + min;
+    if(sec<10) sec = '0' + sec;
+    if(milsec<10) milsec = '0' + milsec;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sec;
+    miliSegundos.textContent = milsec;
+
+})
